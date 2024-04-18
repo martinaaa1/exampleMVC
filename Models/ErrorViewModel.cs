285993899f0150689484f2eae545539fc822ec6f
martinaaa1 <martinastenisca1@gmail.com>
2024-04-18 21:26:11 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exampleMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
